07f991b7a50f921490fa9ff2775ba7da985c3aea
Ramiro Aleman <ralemangzz@gmail.com>
2020-10-25 00:23:55 -0500

    forms

--- js/app.js
+++ js/app.js
@@ -23,3 +23,11 @@ let letter = "";
 
 
 }());
+
+const checkbox =
+      document.getElementById("checkbox");
+
+checkbox.addEventListener("change", () => {
+  //change the theme of the website
+  document.body.classList.toggle("dark");
+} );
