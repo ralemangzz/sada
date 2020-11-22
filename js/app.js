8fbc0020d389aa94e2c281dda357e417439fb352
Ramiro Aleman <ralemangzz@gmail.com>
2020-11-22 00:27:11 -0600

    prueba3 12:26

--- js/app.js
+++ js/app.js
@@ -28,3 +28,19 @@ const checkbox =
       document.getElementById("checkbox");
 
 
+    
+    var setTheme = localStorage.getItem('theme');
+    console.log('theme:', setTheme)
+
+    if(setTheme == null){
+        swapStyle('light.css')
+    }else{
+        swapStyle(setTheme)
+    }
+
+    function swapStyle(sheet){
+        document.getElementById('mystylesheet').href = sheet
+        localStorage.setItem('theme', sheet)
+    }
+
+  
